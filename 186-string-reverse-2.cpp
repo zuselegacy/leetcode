986cd34420ecbf928119bda710a34557d3fb2ae7
sanjith <zuselegacy@gmail.com>
2019-12-27 23:21:13 +0530

    string reverse

---
+++ 186-string-reverse-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void reverse(vector<char>&s, int start, int end) {
+        while(start<end) {
+            char tmp = s[start];
+            s[start] = s[end];
+            s[end]= tmp;
+            start++;
+            end--;
+        }
+    }
+    void reverseWords(vector<char>& s) {
+        reverse(s,0,s.size()-1);
+        int start = 0;
+        for(int i=0;i<s.size();i++) {
+            if(s[i] == ' ' || i==s.size()-1) {
+                reverse(s,start,i-1);
+                start = i + 1;
+            }            
+        }
+        reverse(s,start,s.size()-1);
+    }
+};
