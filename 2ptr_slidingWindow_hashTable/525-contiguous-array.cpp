c6efa75b79220c404c23649882a573f2630626b7
sanjith <zuselegacy@gmail.com>
2020-06-11 18:09:00 +0530

    sokn

--- 2ptr_slidingWindow_hashTable/525-contiguous-array.cpp
+++ 2ptr_slidingWindow_hashTable/525-contiguous-array.cpp
@@ -20,3 +20,27 @@ public:
         return maxLen;        
     }
 };
+
+
+// soln improved - init hashmap with inital condition
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int maxLen=0;
+        map<int,int> sumIndex;
+        int sum=0;
+        sumIndex[0] = -1;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]==0)
+                sum--;
+             else
+                 sum++;
+            if(sumIndex.find(sum)!=sumIndex.end()) {
+                maxLen = max(maxLen, i-sumIndex[sum]);
+            } else {
+                sumIndex[sum] =i;
+            }
+        }
+        return maxLen;
+    }
+};
